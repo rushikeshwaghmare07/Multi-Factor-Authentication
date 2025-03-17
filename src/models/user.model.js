7c6f4f13c9f194e211ab6f62e759ebc5cd4aedd3
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-03-17 23:21:45 +0530

    Add `User Schema Model`

---
+++ src/models/user.model.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isMfaEnabled: {
+    type: Boolean,
+    default: false,
+  },
+  twoFactorAuthSecret: {
+    type: String,
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+export const User = mongoose.model("User", userSchema);
