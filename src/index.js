59785c256e5e654bf4ca64d53e35fae24e0d7035
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-03-17 23:02:33 +0530

    Setup middlewares

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
+import session from "express-session";
+import passport from "passport";
 
 const app = express();
 const PORT = process.env.PORT || 8001;
+
+// Middlewares
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 60000 * 60,
+    },
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
